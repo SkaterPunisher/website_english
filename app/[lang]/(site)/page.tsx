3fd3dfe90fdda26715cd4077259ca18a86335e70
Zykov Artem <skaterpunisher@gmail.com>
2023-08-01 09:36:00 +0300

    preview content on mainPage

--- app/[lang]/(site)/page.tsx
+++ app/[lang]/(site)/page.tsx
@@ -6,6 +6,8 @@ import {
   getPost,
   getPosts,
 } from '@/sanity/schemas/post-schema/post-schema-utils';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export async function generateMetadata({
   params: { lang },
@@ -25,13 +27,33 @@ export default async function IndexPage({
 }: {
   params: { lang: Locale };
 }) {
-  const posts = await getPost('english_for_adults', lang);
-  console.log(posts);
+  const posts = await getPosts(`${lang === 'ru' ? 'ru' : 'en'}`);
 
   return (
     <div>
       <p>Current locale: {lang}</p>
-      {/* <p>Current article: {article.title}</p> */}
+      <div className='mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+        {posts.map((post) => (
+          <Link
+            href={`/project/${post.slug}`}
+            className='border-2 border-gray-500 rounded-lg p-1 hover:scale-105 hover:border-blue-500 transition'
+            key={post._id}
+          >
+            {post.image && (
+              <Image
+                src={post.image}
+                alt={post.name}
+                width={750}
+                height={300}
+                className='object-cover rounded-lg border border-gray-500'
+              />
+            )}
+            <div className='mt-2 font-extrabold bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent'>
+              {post.name}
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
