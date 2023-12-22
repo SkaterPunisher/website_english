951d8c2722c951dc89afc789b20cfe34900bb1a4
Zykov Artem <skaterpunisher@gmail.com>
2023-12-22 08:13:26 +0300

    [fix] errors

--- app/[lang]/(site)/layout.tsx
+++ app/[lang]/(site)/layout.tsx
@@ -2,8 +2,12 @@ import { Locale, i18n } from '@/i18n-config'
 import '@/styles/global.scss'
 import MainLayout from '@/components/shared/MainLayout/MainLayout'
 
+// export async function generateStaticParams() {
+//   return i18n.locales.map(locale => ({ lang: locale }))
+// }
+
 export async function generateStaticParams() {
-  return i18n.locales.map(locale => ({ lang: locale }))
+  return [{ lang: 'en' }, { lang: 'ru' }]
 }
 
 export default async function Root({
--- app/[lang]/(studio)/layout.tsx
+++ app/[lang]/(studio)/layout.tsx
@@ -1,19 +1,16 @@
-import '@/assets/global.scss';
-import type { Metadata } from 'next';
+import '@/styles/global.scss'
+import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'My awesome site',
-  description: 'The best next-sanity-app',
-};
+  title: 'Панель администратора',
+  description:
+    'Панель администратора для добавления, редактирования контента на Advanced English || Zykova Oksana',
+}
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
+    <html lang="ru">
       <body>{children}</body>
     </html>
-  );
+  )
 }
