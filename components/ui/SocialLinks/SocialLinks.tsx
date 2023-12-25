8a2caa515fe883b886ca871a2c95284709d3c9f3
Zykov Artem <skaterpunisher@gmail.com>
2023-12-25 16:50:26 +0300

    [fix] ArticleTabs [add] local Fonts

--- components/ui/SocialLinks/SocialLinks.tsx
+++ components/ui/SocialLinks/SocialLinks.tsx
@@ -9,16 +9,16 @@ import ViberIcon from '@/icons/viber.svg'
 const SocialLinks = ({ className, ...props }: SocialLinksProps) => {
   return (
     <div className={cn(styles.wrapper, className)} {...props}>
-      <a href="https://vk.com/englishinbogorodsk" className={styles.icon}>
+      <a href="https://vk.com/englishinbogorodsk" target="_blank" className={styles.icon}>
         <VKIcon />
       </a>
-      <a href="https://t.me/Advanced_English_Zykova" className={styles.icon}>
+      <a href="https://t.me/Advanced_English_Zykova" target="_blank" className={styles.icon}>
         <TelegramIcon />
       </a>
-      <a href="https://www.instagram.com/englishschool_bg" className={styles.icon}>
+      <a href="https://www.instagram.com/englishschool_bg" target="_blank" className={styles.icon}>
         <InstagramIcon />
       </a>
-      <a href="viber://chat?number=%2B79506253446" className={styles.icon}>
+      <a href="viber://chat?number=%2B79506253446" target="_blank" className={styles.icon}>
         <ViberIcon />
       </a>
     </div>
